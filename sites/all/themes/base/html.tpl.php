f596568fb901e2f92f6cbff2e9ed20d361885e17
Artem <artemhtml@mail.ru>
2014-05-15 15:34:27 +0300

    lending page 1.1

--- sites/all/themes/base/html.tpl.php
+++ sites/all/themes/base/html.tpl.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="<?php print $language->language; ?>" dir="<?php print $language->dir;?>">
 <head>
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
   <?php print $head; ?>
   <title><?php print $head_title; ?></title>
   <?php print $styles; ?>
